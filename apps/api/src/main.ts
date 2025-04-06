93bff7ca4b58304dff41bf8b38b9df4c48a0e99e
sachinnnsingh0510@gmail.com <105480714+me-sachin-singh@users.noreply.github.com>
2025-04-06 22:47:10 +0530

    swagger && graphql/rest User API && Register/Loigin API implemented

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { INestApplication } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app: INestApplication<any> = await NestFactory.create(AppModule)
+  app.enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle('AUTOSPACE | SACHIN SINGH')
+    .setDescription(
+      `The Autospace  API.
+      <h1>Looking for graphql api?</h1>  
+      Go to <a href='/graphql' target='_blank'>/graphql</a>
+      Or,
+      You might also need to use a <a target='_blank' href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:3000/graphql&document=query users{users{uid}}"> Apollo Explorer</a> for greater experience
+    `,
+    )
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build()
+
+  const documentFactory = SwaggerModule.createDocument(app as any, config)
+  SwaggerModule.setup('api', app as any, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
